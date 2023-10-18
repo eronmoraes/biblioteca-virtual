50a89ec3d5f6607d107ba32870cce1fe9a22a089
Bruno <142849905+Bruno-Mrs@users.noreply.github.com>
2023-10-18 17:40:02 -0300

    Update Manga.java

--- codigo/Manga.java
+++ codigo/Manga.java
@@ -1,25 +1,11 @@
 public class Manga extends Produto {
-    private String autor;
-
-    public Manga() {
-        this.autor = "";
-    }
-
-    public Manga(int id, String nome, double preco, int estoque, String categoria, String editora, String autor) {
-        super(id, nome, preco, estoque, categoria, editora);
-        this.autor = autor;
-    }
-
-    public String getAutor() {
-        return autor;
-    }
-
-    public void setAutor(String autor) {
-        this.autor = autor;
+   
+    public Manga(int id, String nome, double preco, int estoque, String autor, String categoria) {
+        super(id, nome, preco, estoque, autor, categoria);
     }
 
     public void exibirInfo() {
-        System.out.println("Mangá - Nome: " + getNome() + " Id: " + getId() + " Preço: " + getPreco() + " Estoque: " + getEstoque() + " Autor: " + autor);
+        System.out.println("Livro - Nome: " + getNome() + " Id: " + getId() + " Preço: " + getPreco() + " Estoque: " + getEstoque() + " Autor: " + getAutor());
     }
 
 }
