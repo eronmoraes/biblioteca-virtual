9abdf13a811eb7f84f33b86f0f7cc59edcb873e4
Bruno <142849905+Bruno-Mrs@users.noreply.github.com>
2023-10-18 17:37:17 -0300

    Create Pedido.java

---
+++ codigo/Pedido.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Pedido {
+  private int idPedido;
+  private Cliente cliente;
+  private List<Produto> produtos = new ArrayList<>();
+
+  public Pedido(int idPedido, Cliente cliente) {
+      this.idPedido = idPedido;
+      this.cliente = cliente;
+  }
+
+  public void adicionarProduto(Produto produto) {
+      produtos.add(produto);
+  }
+
+  public List<Produto> getProdutos() {
+      return produtos;
+  }
+
+  public double calcularTotal() {
+      double total = 0;
+      for (Produto produto : produtos) {
+          total += produto.getPreco();
+          produto.setEstoque(produto.getEstoque() - 1);
+      }
+      return total;
+  }
+
+}
