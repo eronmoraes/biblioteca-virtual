e6bc0fadf027507e7ca1a336900d8a14ab283fdd
Bruno <142849905+Bruno-Mrs@users.noreply.github.com>
2023-10-18 17:36:43 -0300

    Update Main.java

--- codigo/Main.java
+++ codigo/Main.java
@@ -1,38 +1,61 @@
 class Main {
     public static void main(String[] args) {
 
-        Livro livro1 = new Livro(1, "Ladrão de Raio", 20.90, 5, "Aventura", "Rede", "Jeck");
+        //Produtos //
 
-        livro1.exibirInfo();
+        Livro livro1 = new Livro(1, "O Ladrão de raios", 47.68, 9, "Rick Riordan", "Mitologia grega");
+        Livro livro2 = new Livro(2, "O Hobbit", 50.90, 8, "J.R.R. Tolkien", "Literatura fantástica");
 
-        Revista revista1 = new Revista(2, "Veja", 10.90, 7, "", "");
+        HQ hq1 = new HQ(3, "Homem Aranha", 10.47, 15, "Stan Lee", "Super Heróis");
+        HQ hq2 = new HQ(4, "Turma da Mônica", 7.30, 12, "Mauricio de Sousa", " ");
 
-        revista1.exibirInfo();
+        Manga manga1 = new Manga(5, "One Pice", 13.90, 6, "Eiichiro Oda", " ");
+        Manga manga2 = new Manga(6, "Naruto", 11.50, 6, "Masashi Kishimoto", " ");
 
-        HQ hq1 = new HQ(3, "Turma da Monica", 10.00, 10, "", "Mauricio de Sousa Editora", "Mauricio de Sousa");
+        Curso curso1 = new Curso(7, "Logica de Programação", 60.99, 5, "Mauricio", "");
+        Curso curso2 = new Curso(8, "Excel", 19.99, 5, "Juliana", " ");
 
-        hq1.exibirInfo();
+        Revista revista1 = new Revista(9, "Veja", 8.97,5,"André Lopes", "Informação");
+        Revista revista2 = new Revista(10, "Vogue", 35.50,4,"Arthur Baldwin", "Moda");
 
-        Jornal jornal1 = new Jornal(4, "Super", 2.50, 20, "", "Super");
 
-        jornal1.exibirInfo();
+        //Clientes //
 
-        Manga manga1 = new Manga(5, "Teste", 5.00, 6, "Aventura", "", "Jeck");
+        Cliente cliente1 = new Cliente("Carlos", 123456, "10/05/2000");
+        Cliente cliente2 = new Cliente("Rafael", 654321, "15/07/2002");
 
-        manga1.exibirInfo();
 
-        Curso curso1 = new Curso(6, "Logica de Programação", 20.99, 5, "", "", "Mauricio");
+        //1° Pedido //
 
-        curso1.exibirInfo();
+        CarrinhoDeCompras carrinho1 = new CarrinhoDeCompras();
+        carrinho1.adicionarProduto(livro1);
+        carrinho1.adicionarProduto(hq1);
+        carrinho1.adicionarProduto(revista1);
 
-        Cliente clinte1 = new Cliente("Carlos", 654321, "10/05/2000");
+        Pedido pedido1 = new Pedido(1, cliente1);
+        pedido1.adicionarProduto(livro1);
+        pedido1.adicionarProduto(hq1);
+        pedido1.adicionarProduto(revista1);
 
-        clinte1.exibirInfo();
+        System.out.println("\n1° Pedido:\n Itens no Carrinho de Compras: " + carrinho1.getItens().size());
+        System.out.println("Total do Pedido: R$" + pedido1.calcularTotal());
 
-        Funcionario func1 = new Funcionario("Bruno", 123456, "27/12/2002", 1, "Gerente");
 
-        func1.exibirInfo();
+        //2° Pedido //
+    
+        CarrinhoDeCompras carrinho2 = new CarrinhoDeCompras();
+        carrinho2.adicionarProduto(livro2);
+        carrinho2.adicionarProduto(manga1);
+        carrinho2.adicionarProduto(curso1);
 
+        Pedido pedido2 = new Pedido(2, cliente2);
+        pedido2.adicionarProduto(livro1);
+        pedido2.adicionarProduto(manga1);
+        pedido2.adicionarProduto(curso1);
+
+        System.out.println("\n2° Pedido:\nItens no Carrinho de Compras: " + carrinho2.getItens().size());
+        System.out.println("Total do Pedido: R$" + pedido2.calcularTotal());
+        
     }
     
 }
