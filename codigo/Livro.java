790e2097dd26b7d17cff087fa0f525118308d7cd
Bruno <142849905+Bruno-Mrs@users.noreply.github.com>
2023-10-23 16:34:56 -0300

    Update Livro.java

--- codigo/Livro.java
+++ codigo/Livro.java
@@ -1,11 +1,11 @@
 public class Livro extends Produto {
 
-    public Livro(int id, String nome, double preco, int estoque, String autor, String categoria) {
-        super(id, nome, preco, estoque, autor, categoria);
+    public Livro(int id, String nome, double preco, int estoque, String autor) {
+        super(id, nome, preco, estoque, autor);
     }
 
     public void exibirInfo() {
-        System.out.println("Livro - Nome: " + getNome() + " Id: " + getId() + " Preço: " + getPreco() + " Estoque: " + getEstoque() + " Autor: " + getAutor());
+        System.out.println("Livro - " + getNome() + "  Preço: " + getPreco() + "  Estoque: " + getEstoque());
     }
     
 }
