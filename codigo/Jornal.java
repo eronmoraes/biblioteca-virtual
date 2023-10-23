5c066bfc8525f88f395e57c4fa3928cd0e5735ee
Bruno <142849905+Bruno-Mrs@users.noreply.github.com>
2023-10-23 16:36:30 -0300

    Create Jornal.java

---
+++ codigo/Jornal.java
@@ -0,0 +1,11 @@
+public class Jornal extends Produto {
+
+    public Jornal(int id, String nome, double preco, int estoque, String autor) {
+        super(id, nome, preco, estoque, autor);
+    }
+
+    public void exibirInfo() {
+        System.out.println("Jornal - " + getNome() + "  Preço: " + getPreco() + "  Estoque: " + getEstoque() + "\n");
+    }
+
+}
