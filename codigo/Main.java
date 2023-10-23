e574f1e05e6468041d6306315cee8b88a5f2d47b
Bruno <142849905+Bruno-Mrs@users.noreply.github.com>
2023-10-23 16:33:09 -0300

    Update Main.java

--- codigo/Main.java
+++ codigo/Main.java
@@ -1,61 +1,121 @@
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
 
         //Produtos //
 
-        Livro livro1 = new Livro(1, "O Ladrão de raios", 47.68, 9, "Rick Riordan", "Mitologia grega");
-        Livro livro2 = new Livro(2, "O Hobbit", 50.90, 8, "J.R.R. Tolkien", "Literatura fantástica");
+        Livro livro1 = new Livro(1, "O Ladrão de raios", 47.68, 9, "Rick Riordan");
+        Livro livro2 = new Livro(2, "O Hobbit", 50.90, 8, "J.R.R. Tolkien");
 
-        HQ hq1 = new HQ(3, "Homem Aranha", 10.47, 15, "Stan Lee", "Super Heróis");
-        HQ hq2 = new HQ(4, "Turma da Mônica", 7.30, 12, "Mauricio de Sousa", " ");
+        HQ hq1 = new HQ(3, "Homem Aranha", 10.47, 15, "Stan Lee");
+        HQ hq2 = new HQ(4, "Turma da Mônica", 7.30, 12, "Mauricio de Sousa");
 
-        Manga manga1 = new Manga(5, "One Pice", 13.90, 6, "Eiichiro Oda", " ");
-        Manga manga2 = new Manga(6, "Naruto", 11.50, 6, "Masashi Kishimoto", " ");
+        Manga manga1 = new Manga(5, "One Pice", 13.90, 6, "Eiichiro Oda");
+        Manga manga2 = new Manga(6, "Naruto", 11.50, 6, "Masashi Kishimoto");
 
-        Curso curso1 = new Curso(7, "Logica de Programação", 60.99, 5, "Mauricio", "");
-        Curso curso2 = new Curso(8, "Excel", 19.99, 5, "Juliana", " ");
+        Curso curso1 = new Curso(7, "Logica de Programação", 39.99, 10, "Mauricio");
+        Curso curso2 = new Curso(8, "Excel", 19.99, 5, "Juliana");
 
-        Revista revista1 = new Revista(9, "Veja", 8.97,5,"André Lopes", "Informação");
-        Revista revista2 = new Revista(10, "Vogue", 35.50,4,"Arthur Baldwin", "Moda");
+        Revista revista1 = new Revista(9, "Veja", 8.97,5,"André Lopes");
+
+        Jornal jornal1 = new Jornal(10, "O Tempo", 1.50, 20,"Laura Medioli");
 
 
         //Clientes //
 
-        Cliente cliente1 = new Cliente("Carlos", 123456, "10/05/2000");
-        Cliente cliente2 = new Cliente("Rafael", 654321, "15/07/2002");
+        Cliente cliente1 = new Cliente(" ", 0, "00/00/0000");
 
+        Scanner sc = new Scanner(System.in);
 
-        //1° Pedido //
+        System.out.println("Insira seu nome: ");
+        String nome1 = sc.nextLine();
+        cliente1.setNome(nome1);
+
+        System.out.println("Insira seu CPF: ");
+        int cpf1 = sc.nextInt();
+        cliente1.setCpf(cpf1);
+        
+        System.out.println("Insira sua data de nascimento: ");
+        String dn1 = sc.nextLine();
+        cliente1.setDataNasc(dn1);
+        
+
+        System.out.println("\nEscolha seus produtos: ");
+        System.out.print("1. "); livro1.exibirInfo();
+        System.out.print("2. "); livro2.exibirInfo();
+        System.out.print("3. "); hq1.exibirInfo();
+        System.out.print("4. "); hq2.exibirInfo();
+        System.out.print("5. "); manga1.exibirInfo();
+        System.out.print("6. "); manga2.exibirInfo();
+        System.out.print("7. "); curso1.exibirInfo();
+        System.out.print("8. "); curso2.exibirInfo();
+        System.out.print("9. "); revista1.exibirInfo();
+        System.out.print("10. "); jornal1.exibirInfo();
+        
 
         CarrinhoDeCompras carrinho1 = new CarrinhoDeCompras();
-        carrinho1.adicionarProduto(livro1);
-        carrinho1.adicionarProduto(hq1);
-        carrinho1.adicionarProduto(revista1);
-
         Pedido pedido1 = new Pedido(1, cliente1);
-        pedido1.adicionarProduto(livro1);
-        pedido1.adicionarProduto(hq1);
-        pedido1.adicionarProduto(revista1);
 
-        System.out.println("\n1° Pedido:\n Itens no Carrinho de Compras: " + carrinho1.getItens().size());
-        System.out.println("Total do Pedido: R$" + pedido1.calcularTotal());
+        while (true) {
+            System.out.print("Escolha o produto (1/2/3...) ou digite '0' para finalizar: ");
+            int escolha = sc.nextInt();
+            if (escolha == 0) {
+                break;
+            }
 
-
-        //2° Pedido //
-    
-        CarrinhoDeCompras carrinho2 = new CarrinhoDeCompras();
-        carrinho2.adicionarProduto(livro2);
-        carrinho2.adicionarProduto(manga1);
-        carrinho2.adicionarProduto(curso1);
-
-        Pedido pedido2 = new Pedido(2, cliente2);
-        pedido2.adicionarProduto(livro1);
-        pedido2.adicionarProduto(manga1);
-        pedido2.adicionarProduto(curso1);
-
-        System.out.println("\n2° Pedido:\nItens no Carrinho de Compras: " + carrinho2.getItens().size());
-        System.out.println("Total do Pedido: R$" + pedido2.calcularTotal());
+            switch (escolha) {
+                case 1:
+                    carrinho1.adicionarProduto(livro1);
+                    pedido1.adicionarProduto(livro1);
+                    break;
+                case 2:
+                    carrinho1.adicionarProduto(livro2);
+                    pedido1.adicionarProduto(livro2);
+                    break;
+                case 3:
+                    carrinho1.adicionarProduto(hq1);
+                    pedido1.adicionarProduto(hq1);
+                    break;
+                case 4:
+                    carrinho1.adicionarProduto(hq2);
+                    pedido1.adicionarProduto(hq2);
+                    break;
+                case 5:
+                    carrinho1.adicionarProduto(manga1);
+                    pedido1.adicionarProduto(manga1);
+                    break;
+                case 6:
+                    carrinho1.adicionarProduto(manga2);
+                    pedido1.adicionarProduto(manga2);
+                    break;
+                case 7:
+                    carrinho1.adicionarProduto(curso1);
+                    pedido1.adicionarProduto(curso1);
+                    break;
+                case 8:
+                    carrinho1.adicionarProduto(curso2);
+                    pedido1.adicionarProduto(curso2);
+                    break;
+                case 9:
+                    carrinho1.adicionarProduto(revista1);
+                    pedido1.adicionarProduto(revista1);
+                    break;
+                case 10:
+                    carrinho1.adicionarProduto(jornal1);
+                    pedido1.adicionarProduto(jornal1);
+                    break;
+                default:
+                    System.out.println("Escolha inválida. Tente novamente.");
+            }
+        }
         
+        sc.close();
+    
+        System.out.println("\nPedido de " + cliente1.getNome());
+        System.out.println("Itens no Carrinho de Compras: " + carrinho1.getItens().size());
+        System.out.println("Total do Pedido: R$" + pedido1.calcularTotal());
+    
     }
     
 }
