b1233af81efd078930353025342317f338e58187
Bruno <142849905+Bruno-Mrs@users.noreply.github.com>
2023-12-03 17:01:33 -0300

    Update Pedido.java

--- codigo/Pedido.java
+++ codigo/Pedido.java
@@ -2,17 +2,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Pedido {
-  private int idPedido;
-  private Cliente cliente;
   private List<Produto> produtos = new ArrayList<>();
-
-  public Pedido(int idPedido, Cliente cliente) {
-      this.idPedido = idPedido;
-      this.cliente = cliente;
-  }
 
   public void adicionarProduto(Produto produto) {
       produtos.add(produto);
+  }
+
+  public void removerProduto(Produto produto) {
+      produtos.remove(produto);
   }
 
   public List<Produto> getProdutos() {
