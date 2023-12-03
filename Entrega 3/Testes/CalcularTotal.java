38c21a02e9a6286aa5a6f591036bf2288acf5cf0
Bruno <142849905+Bruno-Mrs@users.noreply.github.com>
2023-12-03 16:56:16 -0300

    Update CalcularTotal.java

--- Entrega 3/Testes/CalcularTotal.java
+++ Entrega 3/Testes/CalcularTotal.java
@@ -5,13 +5,13 @@ import static org.junit.Assert.*;
 import static org.junit.Assert.assertEquals;
 
 @Test
-public void () {
+public void CalcularTotal() {
 
   Livro livro1 = new Livro(1, "O Ladrão de raios", 47.68, 9, "Rick Riordan");
   Livro livro2 = new Livro(2, "O Hobbit", 50.90, 8, "J.R.R. Tolkien");
 
   Cliente cliente1 = new Cliente("Bruno", 123, "bruno@gmail.com");
-  Pedido pedido1 = new Pedido(1, cliente1);
+  Pedido pedido1 = new Pedido();
 
   pedido1.adicionarProduto(livro1);
   pedido1.adicionarProduto(livro2);
