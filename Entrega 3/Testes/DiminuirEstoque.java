599609736b0ac4111ebe38e43d3b3b80109c0638
Bruno <142849905+Bruno-Mrs@users.noreply.github.com>
2023-12-03 16:56:55 -0300

    Update DiminuirEstoque.java

--- Entrega 3/Testes/DiminuirEstoque.java
+++ Entrega 3/Testes/DiminuirEstoque.java
@@ -5,12 +5,11 @@ import static org.junit.Assert.*;
 import static org.junit.Assert.assertEquals;
 
 @Test
-public void () {
+public void DiminuirEstoque() {
 
   Livro livro1 = new Livro(1, "O Ladrão de raios", 47.68, 9, "Rick Riordan");
 
-  Cliente cliente1 = new Cliente("Bruno", 123, "bruno@gmail.com");
-  Pedido pedido1 = new Pedido(1, cliente1);
+  Pedido pedido1 = new Pedido();
 
   pedido1.adicionarProduto(livro1);
   pedido1.calcularTotal();
